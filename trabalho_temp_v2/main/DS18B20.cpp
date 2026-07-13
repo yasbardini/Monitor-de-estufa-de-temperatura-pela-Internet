78268fea60b441869c92c599f9007f9834a65d01
Yasmim Tezza Bardini <yasmim.bardini@outlook.com>
2026-07-13 11:17:00 -0300

    Limpeza do main e DS18B2

--- trabalho_temp_v2/main/DS18B20.cpp
+++ trabalho_temp_v2/main/DS18B20.cpp
@@ -1,57 +1,52 @@
-#include "DS18B20.h" // inclui a bib do sensor de temperatura 
-#include "delay.h" // inclui bib de delay
+#include "DS18B20.h"
+#include "delay.h"
 
-#include <stdio.h> // inclui bib de entrada e saída padrão
+#include <stdio.h>
 
-DS18B20::DS18B20(gpio_num_t pino) // construtor da classe DS18B20, recebe o pino do sensor como parâmetro
+DS18B20::DS18B20(gpio_num_t pino)
 {
-    DEBUG("DS18B20:Construtor\n"); // imprime mensagem de debug
-    onewire = new ONEWIRE(pino); // cria objeto onewire com o pino do sensor, para comunicação com o sensor 
+    DEBUG("DS18B20:Construtor\n");
+    onewire = new ONEWIRE(pino);
 }
 
 void DS18B20::programa(void) // função que programa o sensor, configurando a resolução e o modo de operação
 {
-    onewire->reset(); // envia sinal de reset para o sensor
-    // O sensor DS18B20 tem uma memória interna chamada Scratchpad. O comando 0x4E avisa o sensor que vamos escrever nessa memória.
-    onewire->writeByte(0x4E); // envia comando para escrever na memória de configuração do sensor, próximo byte enviado será o valor a ser escrito na memória de configuração
-    onewire->writeByte(0x00); // envia valor para configurar o sensor, neste caso, 0x00 configura o sensor para resolução de 9 bits 
-    //  significa que a temperatura será medida com uma precisão de 0,5°C 
-    onewire->writeByte(0x00); // envia valor para configurar o sensor, neste caso, 0x00 configura o sensor para modo de operação normal, sem alarmes
-    onewire->writeByte(0x7F); // configura o sensor para trabalhar na sua resolução máxima (12 bits), o que nos dá aquela precisão de até 4 casas decimais
+    onewire->reset();
+    onewire->writeByte(0x4E);
+    onewire->writeByte(0x00);
+    onewire->writeByte(0x00);
+    onewire->writeByte(0x7F);
 }
 
-void DS18B20::init(void) // função que inicializa o sensor, realizando a detecção do dispositivo e capturando seu endereço
-{ 
-    uint8_t serial[6], crc; // vetor de 6 bytes para armazenar o número de série do dispositivo e variável para armazenar o valor do CRC (ACK) 
-
+void DS18B20::init(void)
+{
+    uint8_t serial[6], crc;
     printf("*** init***\n");
-    delay_ms(500); // aguarda 500 milissegundos para garantir que o sensor esteja pronto para comunicação
-
+    delay_ms(500);
     printf("INIT\n");
-    if (onewire->reset() == 0) // envia sinal de reset para o barramento 1-Wire, iniciando a comunicação com os dispositivos conectados
+    if (onewire->reset() == 0)
         printf("Detectou escravo na linha\n");
     else
-        printf("Nao detectou escravo\n"); // escravo significa que o sensor está presente e pronto para comunicação, caso contrário, indica que não há dispositivos conectados no barramento
+        printf("Nao detectou escravo\n");
 
-    onewire->writeByte(READ_ROM); // envia comando para ler o endereço do dispositivo conectado no barramento 1-Wire, permitindo que o ESP32 capture o código da família, número de série e CRC (ACK) do sensor
+    onewire->writeByte(READ_ROM);
 
-    printf("Codigo da Familia: %d\n", onewire->readByte()); // lê e imprime o código da família do dispositivo, que identifica o tipo de sensor conectado (DS18B20 tem código de família 0x28)
-    for (uint8_t x = 0; x < 6; x++) // 
+    printf("Codigo da Familia: %d\n", onewire->readByte());
+    for (uint8_t x = 0; x < 6; x++)
         serial[x] = onewire->readByte();
 
     printf("Numero de Serie : %d %d %d %d %d %d\n",
            serial[0], serial[1], serial[2], serial[3], serial[4], serial[5]);
 
-    crc = onewire->readByte(); // lê e armazena o valor do CRC (ACK) do dispositivo, que é usado para verificar a integridade dos dados transmitidos no barramento 1-Wire
+    crc = onewire->readByte();
     printf("CRC= : %d\n", crc);
 
     delay_ms(1000);
 }
 
-void DS18B20::init2(void) // função que realiza uma segunda inicialização do sensor, verificando novamente a presença do dispositivo no barramento 1-Wire
-// o sensor pode não estar pronto para comunicação na primeira inicialização, garantindo que o sensor esteja corretamente detectado antes de prosseguir com a leitura da temperatura
+void DS18B20::init2(void)
 {
-    printf("*** init2***\n"); 
+    printf("*** init2***\n");
     delay_ms(500);
 
     printf("INIT\n");
@@ -63,25 +58,24 @@ void DS18B20::init2(void) // função que realiza uma segunda inicialização do
     delay_ms(1000);
 }
 
-float DS18B20::readTemp(void) // função que lê a temperatura do sensor DS18B20, sem utilizar o endereço do dispositivo, assumindo que há apenas um sensor conectado no barramento 1-Wire
+float DS18B20::readTemp(void)
 {
-    float temperatura; 
+    float temperatura;
     uint8_t a, b, inteira;
     float frac;
 
-    onewire->reset(); // envia sinal de reset para o barramento 1-Wire, iniciando a comunicação com o sensor
-    onewire->writeByte(SKIP_ROM); // envia comando para ignorar a seleção do dispositivo específico, permitindo que o ESP32 se comunique com o sensor conectado no barramento 1-Wire sem precisar fornecer o endereço do dispositivo
-    onewire->writeByte(INICIA_CONVERSAO_TEMP); // envia comando para iniciar a conversão de temperatura no sensor DS18B20, solicitando que o sensor meça a temperatura ambiente e armazene o valor na memória interna
+    onewire->reset();
+    onewire->writeByte(SKIP_ROM);
+    onewire->writeByte(INICIA_CONVERSAO_TEMP);
     delay_ms(1000);
 
     onewire->reset();
-    onewire->writeByte(SKIP_ROM); // envia comando para ignorar novamente a seleção do dispositivo específico, garantindo que a leitura dos dados de temperatura seja realizada apenas do sensor conectado no barramento 1-Wire
-    onewire->writeByte(READ_TEMP_MEMORY); // envia comando para ler os dados de temperatura armazenados na memória interna do sensor DS18B20, permitindo que o ESP32 capture os bytes que representam a temperatura medida pelo sensor
+    onewire->writeByte(SKIP_ROM);
+    onewire->writeByte(READ_TEMP_MEMORY);
 
     a = onewire->readByte();
     b = onewire->readByte();
     printf("a=%u b=%u\n", a, b);
-
 
     inteira = (b << 4) | (a >> 4);
 
--- trabalho_temp_v2/main/main.cpp
+++ trabalho_temp_v2/main/main.cpp
@@ -1,39 +1,39 @@
-#include "freertos/FreeRTOS.h" //  que gerencia tarefas ao mesmo tempo
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h" // controla o pino físico
-#include "esp_log.h" // imprime mensagens de log
+#include "driver/gpio.h"
+#include "esp_log.h"
 
-#include "WiFiManager.h" // gerencia a conexão Wi-Fi
-#include "MQTTManager.h" // gerencia a conexão MQTT
-#include "DS18B20Sensor.h" // gerencia o sensor de temperatura 
+#include "WiFiManager.h"
+#include "MQTTManager.h"
+#include "DS18B20Sensor.h"
 
-static const char *TAG = "MAIN"; // tag para identificar mensagens de log
+static const char *TAG = "MAIN";
 
-static const char *WIFI_SSID = "NICANDA"; // pra conectar no wifi
+static const char *WIFI_SSID = "NICANDA";
 static const char *WIFI_PASS = "Nicole2005!";
-static const char *MQTT_BROKER = "mqtt://broker.emqx.io"; // endereço do broker MQTT
-static const gpio_num_t DS18B20_GPIO = GPIO_NUM_16; // pino 16 conectado no sensor
+static const char *MQTT_BROKER = "mqtt://broker.emqx.io";
+static const gpio_num_t DS18B20_GPIO = GPIO_NUM_16;
 
-extern "C" void app_main(void) // função principal do programa, idf foi escrita em c mas o código ta em c++
+extern "C" void app_main(void)
 {
-    ESP_LOGI(TAG, "app_main iniciado."); // imprime mensagem de log
+    ESP_LOGI(TAG, "app_main iniciado.");
 
-    WiFiManager wifi(WIFI_SSID, WIFI_PASS); // cria objeto wifi com 
-    DS18B20Sensor sensor(DS18B20_GPIO);  // cria objeto sensor com pino 16
-    MQTTManager mqtt(MQTT_BROKER, &sensor); // cria objeto mqtt com endereço do broker e referência para o sensor
+    WiFiManager wifi(WIFI_SSID, WIFI_PASS);
+    DS18B20Sensor sensor(DS18B20_GPIO);
+    MQTTManager mqtt(MQTT_BROKER, &sensor);
 
-    sensor.begin(); // inicializa o sensor de temperatura
+    sensor.begin();
 
-    wifi.begin(); // realiza a conexão Wi-Fi
-    wifi.waitUntilConnected(); // espera até que a conexão Wi-Fi seja estabelecida 
+    wifi.begin();
+    wifi.waitUntilConnected();
 
-    ESP_LOGI(TAG, "Chamando mqtt.begin()..."); // imprime log
-    mqtt.begin(); // realiza a conexão com o protocolo MQTT
+    ESP_LOGI(TAG, "Chamando mqtt.begin()...");
+    mqtt.begin();
     ESP_LOGI(TAG, "mqtt.begin() chamado com sucesso.");
 
-    while (true) // loop infinito para manter o programa em execução
+    while (true)
     {
         mqtt.loop(); // mantém a conexão MQTT ativa e processa mensagens recebidas
-        vTaskDelay(pdMS_TO_TICKS(5000)); // aguarda 5 segundos antes de enviar a próxima leitura de temperatura
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
